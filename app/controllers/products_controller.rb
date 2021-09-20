1acd9499f240ee0e5a4b4977b8140ec70c1df766
Ikemoto-Yu <cosper79@gmail.com>
2021-09-20 16:00:53 +0900

    重複する商品名の解消

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,9 @@
 class ProductsController < ApplicationController
   before_action :search_product, only: [:index, :search]
-  
+
   def index
     @products = Product.all 
+    set_product_column 
   end
 
   def search
@@ -13,6 +14,10 @@ class ProductsController < ApplicationController
 
   def search_product
     @p = Product.ransack(params[:q])  # 検索オブジェクトを生成
+  end
+
+  def set_product_column
+    @product_name = Product.select("name").distinct  # 重複なくnameカラムのデータを取り出す
   end
 
 end
